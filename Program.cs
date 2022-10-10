9509b16162aafb159a375341a67f973fff951819
Syed Faraz Abbas <106167988+SyedFarazAbbas@users.noreply.github.com>
2022-10-10 00:55:42 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// Class Inheritance, Polymorphism ( Dynamic Polymorphism ) and Encapsulation have been used in this Program. 
+// Class Inheritance, Polymorphism ( Dynamic Polymorphism ) and Encapsulation have been used in this Program. 
 using System;
 namespace MyNamespace
 {
@@ -7,37 +7,61 @@ namespace MyNamespace
         public static void Main(string[] args)
         {
             Console.WriteLine("---------------------------------------------");
-            Console.WriteLine("Calculating Gross Salary Using OOP Concepts.");
+            Console.WriteLine("Calculating Gross Salary Using OOP Concepts."); // Introduction of program.
             Console.WriteLine("---------------------------------------------");
             Console.WriteLine("-->\t 'WELCOME to Salary Calculate Software'\t <--");
             Console.WriteLine();
-            
-            Console.WriteLine("If your Engineer Press '1' on Keyboard");
-            Console.WriteLine("If your Manager Press '2' on Keyboard");
-            Console.WriteLine("If your HR Press '3' on Keyboard");
+
+            Console.WriteLine("If your Engineer Press '1' on Keyboard"); // For user info
+            Console.WriteLine("If your Manager Press '2' on Keyboard"); // For user info
+            Console.WriteLine("If your HR Press '3' on Keyboard"); // For user info
             Console.WriteLine();
-            try
+            bool check = true; // Defining variable.
+            while (check) // While Loop will check whether correct number has been pressed or not
             {
-                Console.Write("Kindly Tell us your Designation by pressing corresponding number: ");
-                int a = Convert.ToInt32(Console.ReadLine());
-                if( a==1){
-                    SalarySlip s = new EngineerSalary(); // Creating EngineerSalary object.
-                    Console.WriteLine(s.GetSalary());
-            }
-                if( a==2){
-                    SalarySlip s = new ManagerSalary(); // Creating ManagerSalary object.
-                    Console.WriteLine(s.GetSalary());
-            }
-                if( a==3){
-                    SalarySlip  s = new HrSalary();    // Creating HrSalary object.
-                    Console.WriteLine(s.GetSalary());
-            }
+                try
+                {
+                    Console.Write("Kindly Tell us your Designation by pressing corresponding number: ");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    if (a == 1)
+                    {
+                        SalarySlip s = new EngineerSalary(); // Creating EngineerSalary object.
+                        Console.WriteLine(s.GetSalary());
+                        Console.ReadLine();
+                        break; // Terminate the Loop.
+                    }
+                    if (a == 2)
+                    {
+                        SalarySlip s = new ManagerSalary(); // Creating ManagerSalary object.
+                        Console.WriteLine(s.GetSalary());
+                        Console.ReadLine();
+                        break; // Terminate the Loop.
+                    }
+                    if (a == 3)
+                    {
+                        SalarySlip s = new HrSalary();    // Creating HrSalary object.
+                        Console.WriteLine(s.GetSalary());
+                        Console.ReadLine();
+                        break; // Terminate the Loop.
+                    }
+                    if (a != 1 && a != 2 && a != 3)
+                    {
+                        Console.WriteLine("Enter valid number PLEASE");
+                    }
+
+                }
+                catch (System.Exception ) // It will handle error. if string is entered
+                {
+                    Console.WriteLine("You are either entering character, Special Character or string. Kindly Enter 1 0r 2 0r 3 number.");
+                    
+                    
+                    
+                    
+                }
                 
+
             }
-            catch (System.Exception ex)
-            {
-                  Console.WriteLine("Kindly Enter int number.");
-            }
+            
             
 
             
@@ -56,10 +80,10 @@ namespace MyNamespace
     public class EngineerSalary : SalarySlip  // Child Class ( Class inheritance).
     {
         private int annuallyPaid = 7000;
-        public override string GetSalary() // Polymorphism.
+        public override string GetSalary() // Dynamic Polymorphism.
         {
             int Esalary = baseSalary + annuallyPaid;
-            return $"Annual Gross Salary of Engineer is ${Esalary}."; // String Interpolation.
+            return "Annual Gross Salary of Engineer is $ " + Esalary; // Result
             
         }
         
@@ -68,12 +92,12 @@ namespace MyNamespace
     {
         private int fuel = 250*12; // Making it for annual
         private int medical = 1000*12; // Making it for annual
-        public override string GetSalary() // Polymorphism.
+        public override string GetSalary() //  Dynamic Polymorphism.
         {
             int Gsalary = baseSalary+ fuel + medical;
             int taxDeduct = Gsalary * 17/100;
             int netSalary = Gsalary - taxDeduct;
-            return $" The Annual Gross salary of Manager is ${Gsalary} and Annual Net Salary is ${netSalary}."; // String Interpolation.
+            return " The Annual Gross salary of Manager is " + Gsalary + " and Annual Net Salary is netSalary " + netSalary; // Result
             
             
         }
@@ -82,12 +106,12 @@ namespace MyNamespace
     {
         private int fuel = 150*12; // Making it for annual
         private int medical = 800*12; // Making it for annual
-        public override string GetSalary() // Polymorphism.
+        public override string GetSalary() // Dynamic Polymorphism.
         {
             int salaryHr = baseSalary + fuel + medical;
-            int telephonic = fuel + medical * 10/100;
+            int telephonic = (fuel + medical) * 10/100;
             int HrTotalSalary = salaryHr +telephonic;
-            return $" Total annual Gross Salary of HR is ${HrTotalSalary}"; // String Interpolation.
+            return " Total annual Gross Salary of HR is $ "+HrTotalSalary;  // Result
 
             
         }
