722785f6db307c5f6a1b018c80529f2838611454
Syed Faraz Abbas <106167988+SyedFarazAbbas@users.noreply.github.com>
2022-10-11 06:48:37 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -113,3 +113,115 @@ namespace MyNamespace
         }
     }
 }
+---------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    public class Candidate
+    {
+        public string name;
+        public double cgpa;
+        int age;
+        string city;
+        int num;
+        
+
+        public Candidate(string Name, double Cgpa, int Age, string City, int Num)
+        {
+            name = Name;
+            cgpa = Cgpa;
+            age = Age;
+            city = City;
+            num = Num;
+
+        }
+        public string getInfo()
+        {
+            
+            return ("Name of Candidate is: "+name +". Cgpa is: "+ cgpa +". Age of Candidate is: " +age+" "+". Candidate resides in "+city+ "Score of Candidate "+num );
+
+        }
+        
+
+
+
+    }
+    public class Document
+    {
+
+    }
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            List<Candidate> list = new List<Candidate>();
+            List<String> list2 = new List<string>();
+            Dictionary<string, double> cgpaNames = new Dictionary<string, double>();
+            int count = 0;
+            int count2 = 0; 
+            while (count<2)
+            {
+                Console.Write("Enter your Name: ");
+                string n = Console.ReadLine();
+                Console.Write("Enter your CGPA: ");
+                double c = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter your Age: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter your City: ");
+                string cc = Console.ReadLine();
+                Random rnd = new Random();
+                int nm = rnd.Next(1,100);
+
+                Candidate x = new Candidate(n, c, a, cc,nm);
+                Console.WriteLine(x.getInfo());
+                count += 1;
+                list.Add(x);
+                //
+
+            }
+            for ( count2 = 0; count2 < 2; count2++ )
+            {
+
+                //Console.WriteLine(list[count2].name);
+                
+                cgpaNames.Add(list[count2].name,list[count2].cgpa);
+               
+                //if (list[count2].cgpa >= 3)
+                //{
+                 //   list2.Add(list[count2].name);
+                //}
+
+
+
+
+            }
+            foreach (KeyValuePair<string, double> z in cgpaNames)
+            {
+                if (z.Value >= 3)
+                {
+                    list2.Add(z.Key);
+
+                };
+
+            }
+            
+            for (int j = 0; j < list2.Count; j++)
+            {
+
+              Console.WriteLine(list2[j]);
+           }
+           Console.ReadLine();
+
+
+
+            
+
+
+        }
+    }
+}
